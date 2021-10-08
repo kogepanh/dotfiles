2b39d2d127c18020fa40447961695f2ed732b79f
kogepanh <k.hosono1209@gmail.com>
2021-10-09 00:14:40 +0900

    update

--- install.sh
+++ install.sh
@@ -2,7 +2,21 @@
 
 set -ue
 
-ln -s .vimrc ~/.vimrc
-ln -s .config ~/.config
+DOTFILES_DIR="$HOME/dotfiles"
+
+# ln -s $DOTFILES_DIR/.vimrc ~/.vimrc
+# ln -s .config ~/.config
+
+for f in .??*
+do
+  [[ "$f" == ".git" ]] && continue
+  [[ "$f" == ".github" ]] && continue
+  [[ "$f" == ".gitignore" ]] && continue
+  [[ "$f" == ".DS_Store" ]] && continue
+
+  ln -snf $DOTFILES_DIR/"$f" $HOME/".$f"
+
+  echo "$f"
+done
 
 exit 0
