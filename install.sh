3503a60a4d9f5f55b8e3d97a1caa7dc3337f6922
kogepanh <k.hosono1209@gmail.com>
2021-10-09 09:55:23 +0900

    update

--- install.sh
+++ install.sh
@@ -4,7 +4,20 @@ set -ue
 
 DOT_DIR="~/dotfiles"
 
+has() {
+  type "$1" > /dev/null 2>&1
+}
+
 if [ ! -d $DOT_DIR ]; then
+  if has "git"; then
+      git clone https://github.com/kogepanh/dotfiles.git ${DOT_DIR}
+  else
+    echo "git required"
+    exit 1
+  fi
+
+  cd ${DOT_DIR}
+
   for f in .??*
   do
     [[ "$f" == ".git" ]] && continue
