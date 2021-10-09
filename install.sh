057e5a630fc4f02747a094a09e5c5871fe42434e
kogepanh <k.hosono1209@gmail.com>
2021-10-09 09:35:21 +0900

    update

--- install.sh
+++ install.sh
@@ -2,18 +2,23 @@
 
 set -ue
 
-DOTFILES_DIR=$HOME"/dotfiles"
+DOT_DIR="~/dotfiles"
 
-for f in .??*
-do
-  [[ "$f" == ".git" ]] && continue
-  [[ "$f" == ".github" ]] && continue
-  [[ "$f" == ".gitignore" ]] && continue
-  [[ "$f" == ".DS_Store" ]] && continue
+if if [ ! -d ${DOT_DIR} ]; then
+  for f in .??*
+  do
+    [[ "$f" == ".git" ]] && continue
+    [[ "$f" == ".github" ]] && continue
+    [[ "$f" == ".gitignore" ]] && continue
+    [[ "$f" == ".DS_Store" ]] && continue
 
-  ln -snf $DOTFILES_DIR/"$f" $HOME/"$f"
+    ln -snf "$DOT_DIR/$f" "~/$f"
 
-  echo "$f"
-done
+    echo "$f"
+  done
+else
+  echo "dotfiles already exists"
+  exit 1
+fi
 
 exit 0
