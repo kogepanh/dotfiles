aedc0bbed0e42fde42b6d810a88a102867b2fbc1
kogepanh <k.hosono1209@gmail.com>
2021-10-10 19:59:39 +0900

    update

--- install.sh
+++ install.sh
@@ -2,38 +2,47 @@
 
 set -ue
 
-DOT_DIR=$HOME/dotfiles
-DOT_REPO="https://github.com/kogepanh/dotfiles.git"
-TAR_BALL="https://github.com/kogepanh/dotfiles/archive/main.tar.gz"
-
 has() {
   type "$1" > /dev/null 2>&1
 }
 
-if [ ! -d $DOT_DIR ]; then
-  if has "git"; then
-    git clone $DOT_REPO $HOME
-  else
-    echo "Error: git required"
+symlink() {
+  [ -e "$2" ] || ln -s "$1" "$2"
+}
+
+setup() {
+  DOT_DIR=$HOME/dotfiles
+  DOT_REPO="https://github.com/kogepanh/dotfiles.git"
+  TAR_BALL="https://github.com/kogepanh/dotfiles/archive/main.tar.gz"
+
+  if [ -d "$DOT_DIR" ]; then
+    echo "dotfiles already exists"
+    cd "$DOT_DIR" && git pull --rebase
     exit 1
+  else
+    if has "git"; then
+      git clone "$DOT_REPO" "$HOME"
+    else
+      echo "Error: git required"
+      exit 1
+    fi
+
+    cd $DOT_DIR
+
+    for f in .??*
+    do
+      [[ "$f" == ".git" ]] && continue
+      [[ "$f" == ".github" ]] && continue
+      [[ "$f" == ".gitignore" ]] && continue
+      [[ "$f" == ".editorconfig" ]] && continue
+      [[ "$f" == ".DS_Store" ]] && continue
+
+      ln -snf $DOT_DIR/$f $HOME/$f
+      echo "$f"
+    done
   fi
+}
 
-  cd $DOT_DIR
-
-  for f in .??*
-  do
-    [[ "$f" == ".git" ]] && continue
-    [[ "$f" == ".github" ]] && continue
-    [[ "$f" == ".gitignore" ]] && continue
-    [[ "$f" == ".editorconfig" ]] && continue
-    [[ "$f" == ".DS_Store" ]] && continue
-
-    ln -snf $DOT_DIR/$f $HOME/$f
-    echo "$f"
-  done
-else
-  echo "dotfiles already exists"
-  exit 1
-fi
+setup()
 
 exit 0
