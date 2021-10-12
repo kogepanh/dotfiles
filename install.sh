905878494404b638ba926a19db73558cd2022dd2
kogepanh <k.hosono1209@gmail.com>
2021-10-12 19:09:45 +0900

    update

--- install.sh
+++ install.sh
@@ -1,56 +1,55 @@
 #!/bin/bash
 
-set -ue
+set -eu
 
 function die(){
-    echo $@ >&2
-    exit 1
+  echo $@ >&2
+  exit 1
 }
 
 function has() {
-    type "$1" > /dev/null 2>&1
+  type "$1" > /dev/null 2>&1
 }
 
 function symlink() {
-    [ -e "$2" ] || ln -s "$1" "$2"
+  [ -e "$2" ] || ln -s "$1" "$2"
 }
 
 function setup() {
-    DOT_DIR=$HOME/dotfiles
-    GITHUB_URL="https://github.com/kogepanh/dotfiles.git"
-    TAR_BALL="https://github.com/kogepanh/dotfiles/archive/main.tar.gz"
+  DOT_DIR=$HOME/dotfiles
+  GITHUB_URL="https://github.com/kogepanh/dotfiles.git"
+  TAR_BALL="https://github.com/kogepanh/dotfiles/archive/main.tar.gz"
 
-    if [ -d "$DOT_DIR" ]; then
-        echo "dotfiles already exists"
-        cd "$DOT_DIR" && git pull --rebase
+  if [ -d "$DOT_DIR" ]; then
+    echo "dotfiles already exists"
+    cd "$DOT_DIR" && git pull --rebase
+  else
+    if [has "git"]; then
+      git clone "$GITHUB_URL" "$HOME"
+    elif [has "curl"]; then
+      curl -L "$TAR_BALL"
+      mv -f dotfiles-master "$DOT_DIR"
+    elif [has "wget"]; then
+      wget -O - "$TAR_BALL"
+      mv -f dotfiles-master "$DOT_DIR"
     else
-        if has "git"; then
-            git clone "$GITHUB_URL" "$HOME"
-            elif has "curl" || has "wget"; then
-            if has "curl"; then
-                curl -L "$TAR_BALL"
-                elif has "wget"; then
-                wget -O - "$TAR_BALL"
-            fi | tar zxv
-            mv -f dotfiles-master "$DOT_DIR"
-        else
-            die "curl or wget required"
-        fi
-
-        cd $DOT_DIR
-
-        for f in .??*
-        do
-            [[ "$f" == ".git" ]] && continue
-            [[ "$f" == ".github" ]] && continue
-            [[ "$f" == ".gitignore" ]] && continue
-            [[ "$f" == ".editorconfig" ]] && continue
-            [[ "$f" == ".DS_Store" ]] && continue
-
-            ln -snfv "$DOT_DIR/$f" "$HOME"/"$f"
-            echo "$f"
-        done
+      die "git or curl or wget required"
     fi
+
+    cd $DOT_DIR
+
+    for f in .??*
+    do
+      [[ "$f" == ".git" ]] && continue
+      [[ "$f" == ".github" ]] && continue
+      [[ "$f" == ".gitignore" ]] && continue
+      [[ "$f" == ".editorconfig" ]] && continue
+      [[ "$f" == ".DS_Store" ]] && continue
+
+      ln -snfv "$DOT_DIR/$f" "$HOME"/"$f"
+      echo "$f"
+    done
+  fi
 }
 
 setup()
